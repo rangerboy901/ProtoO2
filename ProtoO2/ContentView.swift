3be344a8ced00e2ceb9c4c311713c7a4d839ab32
Joseph DeWeese <rangerboy901@icloud.com>
2022-10-05 23:57:01 -0500

    Initial Commit

---
+++ ProtoO2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ProtoO2
+//
+//  Created by Joseph Wil;liam DeWeese on 10/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProtoO2/ProtoO2App.swift
@@ -0,0 +1,17 @@
+//
+//  ProtoO2App.swift
+//  ProtoO2
+//
+//  Created by Joseph Wil;liam DeWeese on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ProtoO2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
